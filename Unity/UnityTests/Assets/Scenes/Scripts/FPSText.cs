bbb2326dd072354fce40b7acac084e6c7097ee5e
Yamen Saraiji <mrayyamen@gmail.com>
2019-12-09 13:35:39 +0900

    Adding FPS counter

---
+++ Unity/UnityTests/Assets/Scenes/Scripts/FPSText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSText : MonoBehaviour
+{
+    public UnityEngine.UI.Text text;
+
+    int counter = 0;
+    FrameCounterHelper fps=new FrameCounterHelper();
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fps.AddFrame();
+        text.text = "FPS: "+fps.FPS.ToString()+"/"+ counter.ToString();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            counter++;
+        }
+    }
+}
